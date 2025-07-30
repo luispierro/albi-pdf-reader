6837740aafe4a302d36c1032e8cfedf7cffb8087
Luis Pierro <luisf.pierro@gmail.com>
2025-07-30 07:25:37 +0200

    adds a loading message

--- public/static/script/script.js
+++ public/static/script/script.js
@@ -1,13 +1,32 @@
 document.getElementById('uploadForm').addEventListener('submit', async (e) => {
     e.preventDefault();
-    const formData = new FormData(e.target);
-    const resp = await fetch('/upload', { method: 'POST', body: formData });
-    if (!resp.ok) { alert("Erro ao gerar PDFs"); return; }
-    const blob = await resp.blob();
+    await enviarCSV(e.target);
+});
+
+async function enviarCSV(form) {
+
+    const spinner = document.getElementById("spinner");
+    const formData = new FormData(form);
+
+    try {
+        // Show the spinner
+        spinner.style.display = "block";
+        const resp = await fetch('/upload', { method: 'POST', body: formData });
+        if (!resp.ok) throw new Error('Erro ao gerar PDFs');
+        
+        const blob = await resp.blob();
+        baixarArquivo(blob, 'pdfs_gerados.zip');
+        spinner.style.display = "none";
+    } catch (err) {
+        alert(err.message);
+    }
+}
+
+function baixarArquivo(blob, nomeArquivo) {
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = url;
-    a.download = 'pdfs_gerados.zip';
+    a.download = nomeArquivo;
     a.click();
     URL.revokeObjectURL(url);
-});
+}
